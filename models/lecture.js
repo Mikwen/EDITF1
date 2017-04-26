5497161213c51b5a6b07eb41443953fc38edbf6f
Alexandra Mjaatvedt <sandraem@stud.ntnu.no>
2017-04-26 17:54:56 +0200

    Fixes get lectures

--- models/lecture.js
+++ models/lecture.js
@@ -28,12 +28,29 @@ module.exports.createLecture = function(newLecture, callback){
 	newLecture.save(callback);
 }
 
+removeQuotes = function(str) {
+	return str.replace(/^"(.*)"$/, '$1');
+}
+
 //Finds lecture by subject name and month
-module.exports.getLectureBySubjectAndMonth = function(subject, month, callback){
-	var query = {lectureId: lectureId,
-				month: date.getMonth()
+module.exports.getLectureByCourseAndMonth = function(course, month, year, callback){
+	var pureMonth = Number(removeQuotes(month));
+	var pureYear = Number(removeQuotes(year));
+	var pureCourse = removeQuotes(course);
+	var monthStart = new Date(pureYear, pureMonth, 1);
+	console.log(monthStart);
+	var monthEnd = new Date(pureYear, pureMonth +1, 1);
+    console.log(monthEnd);
+	var query = {course : pureCourse,
+				startTime: {
+					$gte: monthStart, 
+					$lt: monthEnd
+				}
 			};
-	Lecture.findOne(query, callback);
+	console.log(query);
+	Lecture.find(query, function(err, person) {
+						if (err) console.log(err);
+						callback(person)});
 }
 
 
